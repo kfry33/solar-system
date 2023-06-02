2e1050ea1a5a8c195ab2dc71a0c226e3877a87b7
Kasandra Fry <k.fry@phoenixenv.com>
2023-06-02 11:04:13 -0400

    e15 solved

--- exercises/e15.js
+++ exercises/e15.js
@@ -6,9 +6,16 @@ import { data } from "../data/data";
 
 export function getPlanetsWithNoMoons(data) {
   // Your code goes here...
+  return data.planets
+    .filter((planet) => {
+      if (!planet.moons) {
+        return planet;
+      }
+    })
+    .map((planet) => {
+      return planet.name;
+    });
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-15"
