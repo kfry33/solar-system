d26f448da274312d98e242bbe41bbcdab34cb1af
Kasandra Fry <k.fry@phoenixenv.com>
2023-06-02 10:55:33 -0400

    e13 solved

--- exercises/e13.js
+++ exercises/e13.js
@@ -6,9 +6,12 @@ import { data } from "../data/data";
 
 export function getAveragePlanetsTemperature(data) {
   // Your code goes here...
+  return (
+    data.planets.reduce((acc, val) => {
+      return acc + val.avgTemp;
+    }, 0) / data.planets.length
+  );
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-13"
